0a3677c61784337fbfc700e06380e6638e530c89
dnoelia <noeliahdiaz.17@gmail.com>
2022-09-04 21:44:02 -0300

    Agregar filtro de productos

--- js/products.js
+++ js/products.js
@@ -1,3 +1,15 @@
+let ordenarAscendente=true
+ 
+
+
+
+let ordenarPorRelevancia=false
+
+let filtroMinimo=13500
+
+let filtroMaximo=undefined
+
+
 function mostrarProductosCategoria(categoria){
   getJSONData(PRODUCTS_URL+categoria+EXT_TYPE)
     .then(result =>{
@@ -10,9 +22,41 @@ function mostrarProductosCategoria(categoria){
 
     })
 }
+
+function compararProductos(product1,product2){
+  if(ordenarAscendente === false){
+    let temporal=product1
+    product1=product2
+    product2=temporal
+  }
+  let valor1
+  let valor2
+  if (ordenarPorRelevancia===true){
+   valor1=product1.soldCount
+   valor2=product2.soldCount
+  }else{
+    valor1=product1.cost
+    valor2=product2.cost
+  }
+  if(valor1 > valor2 ){
+    return 1
+  }else if (valor1 < valor2){
+    return -1
+  }else{
+    return 0
+  }
+}
+   
+
+
 function mostrarProductos(products){
     let productsListDiv = document.getElementById("products_list")
-    for (let product of products){
+    let productosOrdenados=products
+      .filter((product)=>!(product.cost<filtroMinimo))
+      .filter((product)=>!(product.cost>filtroMaximo))
+      .sort(compararProductos)
+    productsListDiv.innerHTML=""
+    for (let product of productosOrdenados){
         productsListDiv.innerHTML+=`
           <div>
             <h3>${product.name} ${product.currency} ${product.cost}</h3>
@@ -26,6 +70,41 @@ function mostrarProductos(products){
     }
     
 }
+
+function cargarProductos(){
+  mostrarProductosCategoria(localStorage.getItem("catID"))
+}
+
 window.addEventListener("load",function(){
-    mostrarProductosCategoria(localStorage.getItem("catID"))
+    cargarProductos()
+    document.getElementById("precioAsc").addEventListener("click",()=>{
+      ordenarPorRelevancia=false
+      ordenarAscendente=true
+      cargarProductos()
+    })
+    document.getElementById("precioDesc").addEventListener("click",()=>{
+      ordenarPorRelevancia=false
+      ordenarAscendente=false
+      cargarProductos()
+    })
+    document.getElementById("relevanciaDesc").addEventListener("click",()=>{
+      ordenarPorRelevancia=true
+      ordenarAscendente=false
+      cargarProductos()
+    })
+    document.getElementById("filtrarPrecio").addEventListener("click",()=>{
+      let minimo=document.getElementById("filtroPrecioMin").value
+      let maximo=document.getElementById("filtroPrecioMax").value
+      if(minimo===""){
+        filtroMinimo=undefined
+      }else{
+        filtroMinimo=minimo
+      }
+      if(maximo===""){
+        filtroMaximo=undefined
+      }else{
+        filtroMaximo=maximo
+      }
+      cargarProductos()
+    })
 })
